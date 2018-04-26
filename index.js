22da4cc275f1c5fca427ca33da7d2eecf4a8aee4
🦁 Kelvin-Knighton <keltheceo@gmail.com>
2018-04-26 16:49:17 -0700

    added movie & dinner functionality

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let passengerList = [
     { name: "Jane Doloop", paid: false, ticket: "coach" },
     { name: "Dr Evel", paid: true, ticket: "firstclass" },
@@ -9,22 +8,58 @@ let passengerList = [
 function createDrinkOrder (passenger) {
     var orderFunc;
 
-   if (passenger.ticket === "firstclass") {
-       orderFunc = function () {
-           alert("Would you like a cocktail or wine?");
-       };
-   } else {
-       orderFunc = function () {
-           alert("Your choice is cola or water");
-       };
-   }
-   return orderFunc;
+    if (passenger.ticket === "firstclass") {
+        orderFunc = function () {
+            alert("Would you like a cocktail or wine?");
+        };
+    } else {
+        orderFunc = function () {
+            alert("Your choice is cola or water");
+        };
+    }
+    return orderFunc;
+}
+
+function createDinnerOrder (passenger) {
+    var dinnerFunc;
+
+    if (passenger.ticket === "firstclass") {
+        dinnerFunc = function () {
+            alert("Would you like the Lobster & Beef soup or Shrimp Salad?");
+        };
+    } else {
+        dinnerFunc = function () {
+            alert("Would you like a lunchable or half a cold cut sandwhich?");
+        }
+    }
+    return dinnerFunc;
+}
+
+function enjoyMovie (passenger) {
+    var movieFunc;
+
+    if (passenger.ticket === "firstclass") {
+        movieFunc = function () {
+            alert("Would you like Bose Headphone & Trading places or just look up & enjoy the cinema: The Jerk on the big screen");
+        };
+    } else {
+        movieFunc = function () {
+            alert("enjoy the cinema: The Goonies on the big screen");
+        }
+    }
+    return movieFunc;
 }
 
 console.log("roll call");
 
 function serveCustomer (passenger) {
-    createDrinkOrder(passenger);
+    var getDrinkOrderFunction = createDrinkOrder(passenger);
+    var getDinnerFunc = createDinnerOrder(passenger);
+    var getMovieFunc = enjoyMovie(passenger);
+    getDrinkOrderFunction();
+    getDinnerFunc();
+    getMovieFunc();
+
 }
 
 function servePassengers (passengers) {
@@ -33,87 +68,6 @@ function servePassengers (passengers) {
     }
 }
 
+console.log("roll call 2");
 
-// noFlyList(passengerList);
-//
-// function noFlyList (list) {
-//     for (let i = 0; i < list.length; i++) {
-//         let passenger = list[i];
-//         if (passenger.paid === false) {
-//             list.pop();
-//         } else if (passenger.paid === true) {
-//             console.log("The Plane will being it's accent shortly, buckle up grandma");
-//             return true;
-//         }
-//     }
-//     console.log("Roll call: " + passengerList.name);
-//     return false;
-// }
-
-
-
-
-// let tick = true;
-//
-// function ticker () {
-//     if (tick) {
-//         console.log("Tick");
-//         tick = false;
-//     } else {
-//         console.log("Tock");
-//         tick = true;
-//     }
-// }
-// setInterval(ticker, 1000);
-// clearTimeout(ticker);
-//
-// setTimeout(hellBoy, 2000);
-//
-// function logIt() {
-//     console.log("Hey, I'm included too, yippie");
-// }
-// function hellBoy() {
-//     console.log("I like beers & hate cats");
-// }
-
-
-// window.onload = init;
-//
-// function init () {
-//     let map = document.getElementById("map");
-//     map.mousemove = showCoords(event);
-// }
-//
-// function showCoords (event) {
-//     let map = document.getElementById("coords");
-//     let x = event.clientX;
-//     let y = event.clientY;
-//
-//     map.innerHTML = "Map coordinates: " + x + ", " + y;
-// }
-
-
-// window.onload = init;
-//
-// function init () {
-//     let images = document.getElementsByTagName("img");
-//     for (let i = 0; i < images.length; ++i) {
-//         images[i].onclick = showAnswer;
-//     }
-// }
-//
-// function showAnswer (event) {
-//     let image = event.target;
-//     let name = image.id;
-//     name = name + ".jpg";
-//     image.src = name;
-//
-//     setTimeout(reblur, 2000, image);
-//
-// }
-//
-// function reBlur (image) {
-//     let name = image.id;
-//     name = name + "blur.jpg";
-//     image.src = name;
-// }
+servePassengers(passengerList);
